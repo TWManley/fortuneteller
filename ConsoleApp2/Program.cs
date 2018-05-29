ab2ec42712e6792cc61a91e422d25031aad04f3b
twmanley <tmanley1@yahoo.com>
2018-05-29 12:52:45 -0400

    improving

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -10,30 +10,55 @@ namespace Fortune
     {
         static void Main(string[] args)
         {
+            int moneyResult = 0;
             Console.WriteLine("What is your first name");
             string firstname = Console.ReadLine().ToLower();
             Console.WriteLine("What is your last name");
             string lastname = Console.ReadLine().ToLower();
             Console.WriteLine("What is your age");
             int age = int.Parse(Console.ReadLine());
-            if (age % 2 == 0) { Console.WriteLine("you will retire in 10 years"); }
-            else { Console.WriteLine(" you will retire in 45 years"); }
+
+            if (age % 2 == 0)
+            {
+                moneyResult = 10;
+            }
+            else
+            {
+
+                Console.WriteLine(" you will retire in 45 years");
+            }
+
             Console.WriteLine("What is your birth month");
             int birthmonth = int.Parse(Console.ReadLine());
-            if (birthmonth == 1) { Console.WriteLine("you will have 10 dollars!"); }
-            else if (birthmonth == 0) { Console.WriteLine("you will have 0 dollars in the bank"); }
-            else if (birthmonth == 2) { Console.WriteLine("you will have 10 dollars!"); }
-            else if (birthmonth == 3) { Console.WriteLine("you will have 10 dollars!"); }
-            else if (birthmonth == 4) { Console.WriteLine("you will have 10 dollars!"); }
-            else if (birthmonth == 5) { Console.WriteLine("you will have 100 dollars!"); }
-            else if (birthmonth == 6) { Console.WriteLine("you will have 100 dollars!"); }
-            else if (birthmonth == 7) { Console.WriteLine("you will have 100 dollars!"); }
-            else if (birthmonth == 8) { Console.WriteLine("you will have 100 dollars!"); }
+
+            if ((birthmonth == 0) && (birthmonth < 12)) 
+            {
+                Console.WriteLine("0.00");
+            }
+
+            else if ((birthmonth >= 1) && (birthmonth <= 4))
+            {
+                Console.WriteLine("you will have 10 dollars!");
+            }
+
+            else if ((birthmonth >= 5) && (birthmonth <= 8)) { 
+            Console.WriteLine("100"); }
+
+            else if ((birthmonth >= 9) && (birthmonth <= 12))
+            {
+                Console.WriteLine("1000");
+            }
+            
+            
+            
+           
             else if (birthmonth == 9) { Console.WriteLine("you will have 1000 dollars!"); }
             else if (birthmonth == 10) { Console.WriteLine("you will have 1000 dollars!"); }
             else if (birthmonth == 11) { Console.WriteLine("you will have 1000 dollars!"); }
             else if (birthmonth == 12) { Console.WriteLine("you will have 1000 dollars!"); }
             else if (birthmonth > 12) { Console.WriteLine("you will have 0.00 dollars!"); }
+
+
             Console.WriteLine("What is your favorite ROYGBIV color, enter help if you do not understand?");
             string favoritecolor = (Console.ReadLine());
             if (favoritecolor == "help")
@@ -51,8 +76,7 @@ namespace Fortune
             else if (numberofsiblings == 1) { Console.WriteLine("you will retire to the forest!"); }
             else if (numberofsiblings == 2) { Console.WriteLine("you will retire to Kennybunport Maine!"); }
             else if (numberofsiblings == 3) { Console.WriteLine("you will retire to a government funded care facility!"); }
-            Console.WriteLine("{0} {1}" + " you will retire in {2} years " +
-                "with {3} dollars in the bank "+ "and a vacation home in {4}"+ "your mode of trtiansport will be{5}", firstname,lastname,age, birthmonth,numberofsiblings,favoritecolor);
+            Console.WriteLine("{0} {1} you will retire in {2} years with {3} dollars in the bank and a vacation home in {4} your mode of trtiansport will be{5}", firstname,lastname,moneyResult, birthmonth,numberofsiblings,favoritecolor);
 
                
              
